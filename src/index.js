2e629ae40a2c9b6efccf5b1328b036afa0ea9ad2
Hazizau Kanstantsin <47750585+Gazizoff@users.noreply.github.com>
2019-03-20 13:15:54 +0300

    feat: add an object containing the minimum number of coins needed to make change

--- src/index.js
+++ src/index.js
@@ -1,5 +1,24 @@
 // PLEASE DON'T change function name
 module.exports = function makeExchange(currency) {
-    // Your code goes here!
-    // Return an object containing the minimum number of coins needed to make change
+let obj = {};
+if ( currency>=1 && currency<=10000){
+    obj.H = Math.floor(currency / 50);    
+    obj.Q = Math.floor((currency % 50) / 25);
+    obj.D = Math.floor(((currency % 50) % 25) / 10);
+    obj.N = Math.floor((((currency % 50) % 25) % 10) / 5);
+    obj.P = currency % 5;   
+    for ( var key in obj) {
+        if (obj[key]==0) {
+            delete obj[key];
+        }
+    }
+    return obj;
+}
+else if (currency<=0) {
+    return obj;
+}
+else {
+   obj.error = "You are rich, my friend! We don't have so much coins for exchange";
+   return obj;
+}   
 }
